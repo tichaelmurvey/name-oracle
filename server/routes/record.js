b9aa044cc2e410c92e240e1d9475dbee0d089882
Morgan Kerle <morgankerle@gmail.com>
2022-08-27 13:43:25 +1000

    demo db query

--- server/routes/record.js
+++ server/routes/record.js
@@ -3,79 +3,38 @@ const express = require("express");
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
-const recordRoutes = express.Router();
+
+const router = express.Router();
 
 // This will help us connect to the database
 const dbo = require("../db/conn");
 
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
-
-// This section will help you get a list of all the records.
-recordRoutes.route("/record").get(function (req, res) {
-  let db_connect = dbo.getDb("employees");
-  db_connect
-    .collection("records")
-    .find({})
-    .toArray(function (err, result) {
+router.use("/search-amenities/:amenity", (req, res, next) => {
+  const searchTerm = req.params.amenity;
+  const dbConnection = dbo.getDb();
+  dbConnection
+    .collection("listingsAndReviews")
+    .find({ amenities: searchTerm })
+    .toArray((err, result) => {
       if (err) throw err;
       res.json(result);
     });
 });
 
-// This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("records").findOne(myquery, function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  });
-});
+// router.use("/search-amenities/:amenity", (req, res, next) => {
+//   const searchParam = req.params.amenity;
+//   const dbCon = dbo.getDb("sample_airbnb");
+//   dbCon
+//     .collection("listingsAndReviews")
+//     .find({ amenities: searchParam })
+//     .toArray((err, result) => {
+//       if (err) throw err;
+//       console.log(res.json(result));
+//     });
+// });
 
-// This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myobj = {
-    name: req.body.name,
-    position: req.body.position,
-    level: req.body.level,
-  };
-  db_connect.collection("records").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
-});
+// router.use("/", (req, res) => {
+//   res.send("Welcome to my website");
+// });
 
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    $set: {
-      name: req.body.name,
-      position: req.body.position,
-      level: req.body.level,
-    },
-  };
-  db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
-});
-
-// This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
-  });
-});
-
-module.exports = recordRoutes;
+module.exports = router;
